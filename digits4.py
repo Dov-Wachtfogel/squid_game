5342321ce1b59bb67729fc10e4a171850b75186d
Dov-Wachtfogel <dovbeeriv@gmail.com>
2022-09-18 11:13:23 +0300

    end

--- digits4.py
+++ digits4.py
@@ -7,14 +7,27 @@ class digits4:
         self.someone_win = False
 
     def check_num(self, num: int | str):
-        num = str(num)
+        num = list(str(num))
+        T_num = list(self.num)
         A_num = 0
         B_num = 0
-        for i in range(4):
-            if num[i] == self.num[i]:
-                A_num += 1
-            elif num[i] in self.num:
-                B_num += 1
+        try:
+            for i in range(4)[::-1]:
+                if num[i] == T_num[i]:
+                    A_num += 1
+                    num.pop(i)
+                    T_num.pop(i)
+        except:
+            pass
+        try:
+            for i in range(len(num)):
+                for x in range(len(T_num)):
+                    if num[i] == T_num[x]:
+                        B_num += 1
+                        num.pop(i)
+                        T_num.pop(x)
+        except:
+            pass
         if A_num == 4:
             self.someone_win = True
         return f'{A_num}A{B_num}B'
---
+++ main.py
@@ -0,0 +1,5 @@
+import digits4
+
+n = digits4.digits4()
+n.num = '1234'
+print(n.check_num('1233'))
